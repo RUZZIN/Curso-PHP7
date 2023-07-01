9d6c38f65b67f32a9e17656f944c64aae4f3c83a
RUZZIN <125768983+RUZZIN@users.noreply.github.com>
2023-07-01 09:53:42 -0300

    Primeiro envio

---
+++ echo-print.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula - 02</title>
+</head>
+<body>
+    <!--
+        Pode usar tags html dentro das aspas simples ou aspas duplas
+    -->
+    <?php
+
+        echo 'Comando echo </br></hr>';
+    
+        print 'Comando print </br></hr>';
+
+        echo print ('Comando echo e print </br></hr>');
+
+    ?>
+    
+</body>
+</html>
---
+++ tags.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula - 01</title>
+</head>
+<body>
+
+    <?php echo 'Utilizando a tag padrão'; ?>
+
+    <br>
+
+    <?= 'Utilizando a tag impressão' ?>
+
+    <br>
+
+    <? 'Utilizando a tag curta' ?>
+    
+</body>
+</html>
